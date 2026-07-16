a490bd9d1a58fde53bb0fb6780f2861705ddf1e6
RealJeepGuy <hbss93@outlook.com>
2026-07-16 17:03:07 -0600

    Proj1A task2 complete

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+
+
+    public Deque<Character> wordToDeque(String word){
+        Deque<Character> charDeque = new LinkedListDeque<>();
+
+        for (int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            charDeque.addLast(c);
+        }
+        return charDeque;
+
+    }
+
+}
